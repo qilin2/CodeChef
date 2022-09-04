9273072f23ee7278332616e23e09eb3a0fc1916e
Brandon Qilin Hung <93355852+qilin2@users.noreply.github.com>
2022-09-04 09:49:38 -0700

    Update 158B.cpp

--- codeforces/Problems/1000-1999_Pupil/158B.cpp
+++ codeforces/Problems/1000-1999_Pupil/158B.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-    int n;
+    	int n;
 	cin>>n;
-	int c1=0, c2=0, c3=0, c4=0, cn=0;
+	int c1=0, c2=0, c3=0, c4=0;
 	vector<int>v;
 	for(int i=0; i<n; i++)
-    {
+    	{
 		int s;
 		cin>>s;
-		cn+=s;
 		if(s==4)    c4++;
 		if(s==3)    c3++;
 		if(s==2)    c2++;
-		if(s==1)	c1++;
+		if(s==1)    c1++;
 	}
 	c4+=c3;
 	c1-=c3;
 	c4+=c2/2;
 	c2=c2%2;
-	if(c2==1){
+	if(c2==1)
+	{
 		c1-=2;
 		c2=0;
 		c4++;
